beab687695e56dee76ce76a8322de1ee40b05413
Charles Parker <chparker@microsoft.com>
2017-06-16 13:13:01 -0700

    a few tweaks

--- server/src/blappSync.js
+++ server/src/blappSync.js
@@ -1,4 +1,12 @@
 "use strict";
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var io = require("socket.io");
 var uuid = require("uuid");
@@ -66,7 +74,7 @@ function init(server) {
                 return;
             }
             session.sharedVars[data.name] = data.value;
-            ios.to(sid).emit('shareVarUpdated', Object.assign({ serverTime: new Date().getTime() }, data));
+            ios.to(sid).emit('shareVarUpdated', __assign({ serverTime: new Date().getTime() }, data));
         });
     });
 }
