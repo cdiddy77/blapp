150e77a1a2d30ef47a2d2594bfdf1e464c902372
Charles Parker <charles@parkerfolk.net>
2017-06-18 18:27:20 -0700

    added downloadable APK

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,14 +8,28 @@ import * as blappSync from './blappSync';
 var rootDir = "build/";
 
 var server = http.createServer(function (req, res) {
-    var urlObj = url.parse(req.url, true, false);
-    var pathName = urlObj.pathname == '/' ? 'index.html' : urlObj.pathname;
+    let urlObj = url.parse(req.url, true, false);
+    let isApk: boolean = false;
+    let pathName: string;
+    if (urlObj.pathname == '/') {
+        pathName = 'index.html';
+    } else if (urlObj.pathname == '/downloadapk') {
+        pathName = 'app-release.apk';
+        isApk = true;
+    } else {
+        pathName = urlObj.pathname;
+    };
+
     fs.readFile(rootDir + pathName, function (err, data) {
         if (err) {
             console.log('error trying to serve with pathname=', urlObj.pathname);
             res.writeHead(404);
             res.end(JSON.stringify(err));
             return;
+        }
+        if (isApk) {
+            res.setHeader('Content-Disposition', 'attachment; filename="blockapps.apk"');
+            res.setHeader('Content-Type', 'application/vnd.android.package-archive');
         }
         res.writeHead(200);
         res.end(data);
